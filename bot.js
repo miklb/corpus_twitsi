1f53e9ffae0a83cb5a9539ff1d5d3d7958604dee
Michael Bishop <miklb@miklb.com>
2019-07-17 21:50:14 -0400

    remove timeout
    Signed-off-by: Michael Bishop <miklb@miklb.com>

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ var Bot = new CorpusBot({
     consumer_key: process.env.BOT_CONSUMER_KEY,
     consumer_secret: process.env.BOT_CONSUMER_SECRET,
     access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
-    timeout_ms: 60*1000
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
    });
 
    var retweet = function() {
